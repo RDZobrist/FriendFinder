4fa9835439e75e687c0e37cdbe05b11a8c102d5f
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-22 09:56:48 -0700

    added herku as remote repository

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ var app = express();
 var PORT = process.env.PORT || 8088;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // ================================================================================
 // ROUTER
@@ -38,5 +38,5 @@ require("./app/routing/htmlRoutes")(app);
 // ==============================================================================
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: " + PORT);
 });
